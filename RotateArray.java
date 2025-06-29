d62af0ca23221ec374e84934a91fbc90ccb0a6b6
HP <saurkumar1994@gmail.com>
2025-06-29 22:33:24 +0530

    SKY(Rotate arr)

--- RotateArray.java
+++ RotateArray.java
@@ -1,3 +1,28 @@
 public class RotateArray {
     
+      static void rotateArr(int[] arr, int d) {
+        int n = arr.length;
+  
+        // Repeat the rotation d times
+        for (int i = 0; i < d; i++) {
+          
+            // Left rotate the array by one position
+            int first = arr[0];
+            for (int j = 0; j < n - 1; j++) {
+                arr[j] = arr[j + 1];
+            }
+            arr[n - 1] = first;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int d = 2;
+
+        rotateArr(arr, d);
+
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+
 }
